3f5e8d9482525b195c9584f23999f32b168823f5
inbarkarmy <inbarkarmy@campus.technion.ac.il>
2018-06-11 11:10:50 +0300

    start

---
+++ app/src/main/java/com/example/inbar/suitcaseapp/AuthenticatorActivity.java
@@ -0,0 +1,38 @@
+package com.example.inbar.suitcaseapp;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.amazonaws.mobile.auth.google.GoogleButton;
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+public class AuthenticatorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator);
+
+        // Add a call to initialize AWSMobileClient
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                AuthUIConfiguration config =
+                        new AuthUIConfiguration.Builder()
+                                .signInButton(GoogleButton.class) // Show Google button
+                                .logoResId(R.drawable.paddington_hero) // Change the logo
+                                .backgroundColor(Color.YELLOW) // Change the backgroundColor
+                                .isBackgroundColorFullScreen(true) // Full screen backgroundColor the backgroundColor full screenff
+                                .fontFamily("sans-serif-light") // Apply sans-serif-light as the global font
+                                .canCancel(true)
+                                .build();
+                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+                signin.login(AuthenticatorActivity.this, MainActivity.class).authUIConfiguration(config).execute();
+            }
+        }).execute();
+    }
+}
---
+++ app/src/main/java/com/example/inbar/suitcaseapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.inbar.suitcaseapp;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInUI;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+    }
+}
